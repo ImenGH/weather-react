027e8123cbc50279d695a1a694da8261c9c78d62
Imen Grab <ghrabimene@gmail.com>
2020-10-22 22:31:26 +0200

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `the temperature in ${response.data.name} is ${response.data.main.temp}`
+    );
+  }
+  let apiKey = "b016d3139dfb068d018e3bb03da1b5f3";
+  let units = "metric";
+  let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+  axios.get(urlApi).then(handleResponse);
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
+}
