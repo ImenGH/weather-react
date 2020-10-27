6bfeaf450582a6ba35dda124d4a6f631f21d7dfb
Imen Grab <ghrabimene@gmail.com>
2020-10-27 22:12:22 +0100

    Add Footer.css and github link

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,22 @@
 import React from "react";
+import "./Footer.css";
+
 export default function Footer() {
   return (
     <div>
       <small className="footer">
         Created by
-        <a href="https://www.linkedin.com/in/imen-grab-0b2562127/" id="name">
+        <a
+          href="https://www.linkedin.com/in/imen-grab-0b2562127/"
+          target="_blank"
+          id="name"
+        >
           Imen Grab
         </a>
         <br />
         <a
-          href="https://github.com/ImenGH/Awsome-weather-app"
+          href="https://github.com/ImenGH/weather-react"
+          target="_blank"
           className="github"
         >
           <i className="fab fa-github" id="source">
