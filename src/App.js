bfa5804cc133fe506a108c9a3acb266ca880ae55
Imen Grab <ghrabimene@gmail.com>
2020-10-30 22:17:07 +0100

    Added Api integration

--- src/App.js
+++ src/App.js
@@ -8,29 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather
-          city="Tunis"
-          temperature={10}
-          description="Cler sky"
-          humidity={40}
-          wind={2}
-          month="October"
-          date={25}
-          day="Saturday"
-          hour="12 : 00"
-          day1="Sunday"
-          day2="Monday"
-          day3="Tuesday"
-          day4="Wednesday"
-          tempmax1={20}
-          tempmax2={18}
-          tempmax3={15}
-          tempmax4={17}
-          tempmin1={17}
-          tempmin2={15}
-          tempmin3={11}
-          tempmin4={14}
-        />
+        <Weather defaultCity="tokyo" />
         <Footer />
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,164 +1,187 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 import cloudy from "./images/cloudy.svg";
 import sunny from "./images/sunny.svg";
 import rain from "./images/rain.svg";
 
 export default function Weather(props) {
-  return (
-    <div className="weather">
-      <form id="real-city" className="mb-2">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              id="city-input"
-              autoComplete="off"
-              autoFocus="on"
-            />
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response);
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].main,
+      iconUrl: "https:/ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="weather">
+        <form id="real-city" className="mb-2">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                id="city-input"
+                autoComplete="off"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="🔍 Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+
+        <div className="row currentWeather">
+          <div className="col-4">
+            <div className="clearfix">
+              <h1 id="city">{weatherData.city}</h1>
+              <h4 id="visibility" className="text-capitalize">
+                {weatherData.description}
+              </h4>
+              <br />
+              <div>
+                <span className="temperature" id="temp">
+                  {weatherData.temperature}
+                </span>
+                <a href="NAN" id="celsius" className="active">
+                  °C
+                </a>{" "}
+                |
+                <a href="NAN" id="fahrenheit">
+                  °F
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <img src={weatherData.iconUrl} alt={weatherData.description}></img>
+          </div>
+
+          <div className="col-4">
+            <div className="row precipitation">
+              <div className="col-12">
+                <span
+                  className="iconify"
+                  data-icon="wi-humidity"
+                  data-inline="false"
+                ></span>
+                <span id="humidity">{weatherData.humidity}</span> %
+              </div>
+
+              <div className="col-12">
+                <span
+                  className="iconify"
+                  data-icon="bx:bx-wind"
+                  data-inline="false"
+                ></span>
+                <span id="windy">{weatherData.wind}</span> m/s
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row date">
+          <div className="col-4">
+            <span id="actual-month">month</span>
+            <span id="actual-date"> date</span>
+          </div>
+          <div className="col-4">
+            <span id="today">day</span>
+          </div>
+          <div className="col-4">
+            <span id="actual-hour">hour</span>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row weekWeather">
+          <div className="col-3">
+            <div className="friday">
+              <h5 id="day-one">day1</h5>
+
+              <img
+                className="cloudy weather-icon"
+                src={cloudy}
+                alt="cloudy"
+                id="img-day1"
+              />
+              <div>
+                <b id="max-1">tempmax1</b>°/ <span id="min-1">tempmin1</span>°
+              </div>
+            </div>
           </div>
           <div className="col-3">
-            <input
-              type="submit"
-              value="🔍 Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
+            <div className="saturday">
+              <h5 id="day-two">day2</h5>
 
-      <div className="row currentWeather">
-        <div className="col-4">
-          <div className="clearfix">
-            <h1 id="city">{props.city}</h1>
-            <h4 id="visibility">{props.description}</h4>
-            <br />
-            <div>
-              <span className="temperature" id="temp">
-                {props.temperature}
-              </span>
-              <a href="NAN" id="celsius" className="active">
-                °C
-              </a>{" "}
-              |
-              <a href="NAN" id="fahrenheit">
-                °F
-              </a>
+              <img
+                className="cloudy weather-icon"
+                src={rain}
+                alt="cloudy"
+                id="img-day2"
+              />
+              <div>
+                <b id="max-2">tempmax2</b>°/ <span id="min-2">tempmin2</span>°
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-4">
-          <i className="fas fa-circle" id="climat"></i>
-        </div>
+          <div className="col-3">
+            <div className="sunday">
+              <h5 id="day-three">day3</h5>
 
-        <div className="col-4">
-          <div className="row precipitation">
-            <div className="col-12">
-              <span
-                className="iconify"
-                data-icon="wi-humidity"
-                data-inline="false"
-              ></span>
-              <span id="humidity">{props.humidity}</span> %
+              <img
+                className="sunny weather-icon"
+                src={sunny}
+                alt="sunny"
+                id="img-day3"
+              />
+              <div>
+                <b id="max-3">tempmax3</b>°/ <span id="min-3">tempmin3</span>°
+              </div>
             </div>
+          </div>
+          <div className="col-3">
+            <div className="monday">
+              <h5 id="day-four">day4</h5>
 
-            <div className="col-12">
-              <span
-                className="iconify"
-                data-icon="bx:bx-wind"
-                data-inline="false"
-              ></span>
-              <span id="windy">{props.wind}</span> m/s
+              <img
+                className="cloudy weather-icon"
+                src={cloudy}
+                alt="cloudy"
+                id="img-day4"
+              />
+              <div>
+                <b id="max-4">tempmax4</b>°/ <span id="min-4">tempmin4</span>°
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <br />
-      <br />
-      <div className="row date">
-        <div className="col-4">
-          <span id="actual-month">{props.month}</span>
-          <span id="actual-date"> {props.date}</span>
-        </div>
-        <div className="col-4">
-          <span id="today">{props.day}</span>
-        </div>
-        <div className="col-4">
-          <span id="actual-hour">{props.hour}</span>
-        </div>
-      </div>
+    );
+  } else {
+    const apiKey = "b016d3139dfb068d018e3bb03da1b5f3";
+    let units = "metric";
 
-      <hr />
+    let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    axios.get(urlApi).then(handleResponse);
 
-      <div className="row weekWeather">
-        <div className="col-3">
-          <div className="friday">
-            <h5 id="day-one">{props.day1}</h5>
-
-            <img
-              className="cloudy weather-icon"
-              src={cloudy}
-              alt="cloudy"
-              id="img-day1"
-            />
-            <div>
-              <b id="max-1">{props.tempmax1}</b>°/{" "}
-              <span id="min-1">{props.tempmin1}</span>°
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="saturday">
-            <h5 id="day-two">{props.day2}</h5>
-
-            <img
-              className="cloudy weather-icon"
-              src={rain}
-              alt="cloudy"
-              id="img-day2"
-            />
-            <div>
-              <b id="max-2">{props.tempmax2}</b>°/{" "}
-              <span id="min-2">{props.tempmin2}</span>°
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="sunday">
-            <h5 id="day-three">{props.day3}</h5>
-
-            <img
-              className="sunny weather-icon"
-              src={sunny}
-              alt="sunny"
-              id="img-day3"
-            />
-            <div>
-              <b id="max-3">{props.tempmax3}</b>°/{" "}
-              <span id="min-3">{props.tempmin3}</span>°
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="monday">
-            <h5 id="day-four">{props.day4}</h5>
-
-            <img
-              className="cloudy weather-icon"
-              src={cloudy}
-              alt="cloudy"
-              id="img-day4"
-            />
-            <div>
-              <b id="max-4">{props.tempmax4}</b>°/{" "}
-              <span id="min-4">{props.tempmin4}</span>°
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    return "loading...";
+  }
 }
